2c9cf2cd54c44c3823a9f9127062332c36ed5515
Brandon <brpeterman@gmail.com>
2026-01-23 11:20:55 -0600

    Format works.ts

--- src/works.ts
+++ src/works.ts
@@ -1,93 +1,109 @@
 import type { Artwork } from "./index";
 
-export const Commissions: Artwork[] = [{
-  id: "mako",
-  title: "Mako",
-  year: "2025",
-  medium: "Oil pastel on paper",
-  size: "11\" x 8\"",
-  description: "Commissioned work.",
-  imageIds: ["mako"]
-}, {
-  id: "clyde",
-  title: "Clyde",
-  year: "2025",
-  medium: "Pastel on paper",
-  size: "8\" x 10\"",
-  description: "Commission of a cat named Clyde.",
-  imageIds: ["clyde"]
-}, {
-  id: "dual-cats",
-  title: "Cat Commission",
-  year: "2022",
-  medium: "Pastel on paper",
-  size: "8\" x 10\"",
-  description: "A pair of images commissioned as a gift.",
-  imageIds: ["rachel-cat-1", "rachel-cat-2"]
-}];
+export const Commissions: Artwork[] = [
+  {
+    id: "mako",
+    title: "Mako",
+    year: "2025",
+    medium: "Oil pastel on paper",
+    size: '11" x 8"',
+    description: "Commissioned work.",
+    imageIds: ["mako"],
+  },
+  {
+    id: "clyde",
+    title: "Clyde",
+    year: "2025",
+    medium: "Pastel on paper",
+    size: '8" x 10"',
+    description: "Commission of a cat named Clyde.",
+    imageIds: ["clyde"],
+  },
+  {
+    id: "dual-cats",
+    title: "Cat Commission",
+    year: "2022",
+    medium: "Pastel on paper",
+    size: '8" x 10"',
+    description: "A pair of images commissioned as a gift.",
+    imageIds: ["rachel-cat-1", "rachel-cat-2"],
+  },
+];
 
-export const Works: Artwork[] = [{
-  id: "parrots",
-  title: "Parrots in Flight",
-  year: "2025",
-  medium: "Chalk on concrete",
-  size: "5' x 5'",
-  description: "First-place winner at the 2025 ArtInfusion chalk art competition in Janesville, Wisconsin. Theme: \"Let Words Take Flight!\"",
-  imageIds: ["parrots", "parrots-wide"]
-},{
-  id: "starry-night",
-  title: "Starry Night Chameleon",
-  year: "2024",
-  medium: "Chalk on concrete",
-  size: "5' x 5'",
-  description: "First-place winner at the 2024 ArtInfusion chalk art competition in Janesville, Wisconsin. Theme: \"Live Your Art!\"",
-  imageIds: ["starry-night-chameleon", "starry-night-chameleon-wide"]
-}, {
-  id: "globe-frog",
-  title: "Globe Frog",
-  year: "2025",
-  medium: "Chalk on concrete",
-  size: "5' x 5'",
-  description: "Entry for the 2025 Wausau Chalkfest.",
-  imageIds: ["globe-frog", "globe-frog-wide"]
-}, {
-  id: "self-image",
-  title: "Self Image",
-  year: "2025",
-  medium: "Acrylic on fiberboard",
-  size: "32\" x 23\"",
-  description: "Personal project.",
-  imageIds: ["self-image"]
-}, {
-  id: "hop-frog",
-  title: "Hop Frog",
-  year: "2025",
-  medium: "Acrylic on concrete",
-  size: "26' x 5'",
-  description: "This mural is an accent piece for my vegetable garden.",
-  imageIds: ["hop-frog", "hop-frog-wide"]
-}, {
-  id: "mooneater",
-  title: "Mooneater",
-  year: "2025",
-  medium: "Acrylic on fiberboard",
-  size: "24\" x 48\"",
-  description: "Personal project.",
-  imageIds: ["mooneater", "mooneater-wide"]
-}, {
-  id: "lynx",
-  title: "Lynx",
-  year: "2025",
-  medium: "Acrylic on fiberboard",
-  size: "48\" x 69\"",
-  description: "This was a one-day project to use up an old wood veneer panel that had been kicking around my garage.",
-  imageIds: ["lynx", "lynx-wide"]
-}, {
-  id: "king-juju",
-  title: "King Juju",
-  year: "2023",
-  medium: "Pastel on paper",
-  size: "7\" x 10\"",
-  description: "Sketch in memoriam of a friend's cat.",
-  imageIds: ["king-juju"]
-}];
+export const Works: Artwork[] = [
+  {
+    id: "parrots",
+    title: "Parrots in Flight",
+    year: "2025",
+    medium: "Chalk on concrete",
+    size: "5' x 5'",
+    description:
+      'First-place winner at the 2025 ArtInfusion chalk art competition in Janesville, Wisconsin. Theme: "Let Words Take Flight!"',
+    imageIds: ["parrots", "parrots-wide"],
+  },
+  {
+    id: "starry-night",
+    title: "Starry Night Chameleon",
+    year: "2024",
+    medium: "Chalk on concrete",
+    size: "5' x 5'",
+    description:
+      'First-place winner at the 2024 ArtInfusion chalk art competition in Janesville, Wisconsin. Theme: "Live Your Art!"',
+    imageIds: ["starry-night-chameleon", "starry-night-chameleon-wide"],
+  },
+  {
+    id: "globe-frog",
+    title: "Globe Frog",
+    year: "2025",
+    medium: "Chalk on concrete",
+    size: "5' x 5'",
+    description: "Entry for the 2025 Wausau Chalkfest.",
+    imageIds: ["globe-frog", "globe-frog-wide"],
+  },
+  {
+    id: "self-image",
+    title: "Self Image",
+    year: "2025",
+    medium: "Acrylic on fiberboard",
+    size: '32" x 23"',
+    description: "Personal project.",
+    imageIds: ["self-image"],
+  },
+  {
+    id: "hop-frog",
+    title: "Hop Frog",
+    year: "2025",
+    medium: "Acrylic on concrete",
+    size: "26' x 5'",
+    description: "This mural is an accent piece for my vegetable garden.",
+    imageIds: ["hop-frog", "hop-frog-wide"],
+  },
+  {
+    id: "mooneater",
+    title: "Mooneater",
+    year: "2025",
+    medium: "Acrylic on fiberboard",
+    size: '24" x 48"',
+    description: "Personal project.",
+    imageIds: ["mooneater", "mooneater-wide"],
+  },
+  {
+    id: "lynx",
+    title: "Lynx",
+    year: "2025",
+    medium: "Acrylic on fiberboard",
+    size: '48" x 69"',
+    description:
+      "This was a one-day project to use up an old wood veneer panel that had been kicking around my garage.",
+    imageIds: ["lynx", "lynx-wide"],
+  },
+  {
+    id: "king-juju",
+    title: "King Juju",
+    year: "2023",
+    medium: "Pastel on paper",
+    size: '7" x 10"',
+    description: "Sketch in memoriam of a friend's cat.",
+    imageIds: ["king-juju"],
+  },
+];
